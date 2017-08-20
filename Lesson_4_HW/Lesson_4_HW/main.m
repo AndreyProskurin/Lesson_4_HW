45478f5de2c5f52cdb8951b34849b066b1f88ad9
Andrey Proskurin <andrewproskurin@gmail.com>
2017-08-20 22:03:23 +0300

    Добавил проект.

---
+++ Lesson_4_HW/Lesson_4_HW/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Lesson_4_HW
+//
+//  Created by Andrey Proskurin on 20.08.17.
+//  Copyright © 2017 Andrey Proskurin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
